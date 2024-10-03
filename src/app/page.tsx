dfdd95193814bd74d8ca4f65ea1dea7a68a0de10
maisa0804 <breitmeer.0804@gmail.com>
2024-10-03 21:18:07 +0900

    Added links and my photo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,13 @@ export default async function Home() {
 
   return (
     <>
-      <Hero/>
+      <div className="flex px-10 justify-start items-center my-12">
+        <div>
+          <h1 className="font-bold">Demo 'n' Code</h1>
+          <p className="leading-8">Welcome to my workplace and library. I dump my works in here to share my knowledge and skill with you. I would love to collab, work, have fun with anybody. (Posts are created by <span><a href="https://www.notion.so/product" target="_brank" className="underline">Notion database</a></span>.)</p>
+        </div>
+      </div>
+      <Hero />
     </>
   );
 }
